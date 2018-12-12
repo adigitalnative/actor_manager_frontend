7c42c4c3761addb9e9da9b69c03e9471b6b6b7cc
Jacqueline Chenault <adigitalnative@gmail.com>
2018-12-12 14:39:33 -0500

    WIP Audition categories into form

--- src/components/AuditionForm.js
+++ src/components/AuditionForm.js
@@ -1,12 +1,20 @@
 import React, { Component, Fragment } from 'react'
-import { Modal, Form, Button } from 'semantic-ui-react'
+import { Modal, Form, Button, Select } from 'semantic-ui-react'
+
+const categoryOptions = [
+  {key: "1", text: "Open Call", value: "1"},
+  {key: "2", text: "EPA", value: "2"},
+  {key: "4", text: "Callback", value: "4"},
+]
 
 class AuditionForm extends Component {
+
   constructor() {
     super()
     this.state = {
       bring: "",
-      prepare: ""
+      prepare: "",
+      auditionCategory: null
     }
   }
 
@@ -24,6 +32,7 @@ class AuditionForm extends Component {
         <Modal.Header>Create an Audition</Modal.Header>
         <Modal.Content>
           <Form onSubmit={this.handleSubmit}>
+            <Form.Field control={Select} label='Category' options={categoryOptions} placeholder='Audition Category' value={this.state.auditionCategory} name='auditionCategory' onChange={this.handleChange}/>
             <Form.Group widths="equal">
               <Form.Input label="Bring" type="text" name="bring" onChange={this.handleChange} value={this.state.bring}/>
               <Form.Input label="Prepare" type="text" name="prepare" onChange={this.handleChange} value={this.state.prepare}/>
